eb9c150fa58c0262d084c89e0c5d9165b8134365
itallosavieira <itallo.sa.vieira@gmail.com>
2020-09-21 23:01:06 -0300

    Implementacao: class Funcionario

--- projetos/02_javascript-interfaces-heranca-oo/index.js
+++ projetos/02_javascript-interfaces-heranca-oo/index.js
@@ -1,9 +1,7 @@
 import { Cliente } from './Cliente.js'
-import { ContaCorrente } from './ContaCorrente.js'
-import { ContaPoupanca } from './ContaPoupanca.js'
-import { Conta } from './Conta.js'
-import { ContaSalario } from './ContaSalario.js';
-
+import { ContaCorrente } from './Conta/./ContaCorrente.js'
+import { ContaPoupanca } from './Conta/./ContaPoupanca.js'
+import { ContaSalario } from './Conta/./ContaSalario.js';
 
 const cliente1 = new Cliente("Itallo", 44455599900);
 const contaCorrente = new ContaCorrente(cliente1, 1001);
@@ -12,10 +10,8 @@ const contaSalario = new ContaSalario(cliente1);
 contaCorrente.depositar(1000);
 contaCorrente.sacar(100);
 contaPoupanca.sacar(100);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-
+contaSalario.depositar(1000);
+contaSalario.sacar(100);
 
 console.log(contaCorrente);
 console.log(contaPoupanca);
